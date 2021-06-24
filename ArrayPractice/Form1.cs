1a09047ec73d7f96ec26d7c8841b7334b2437ad7
A601_03 <tnk.dat.class@gmail.com>
2021-06-24 09:46:34 +0900

    個別に動かす、別の速度で動かす、それぞれフォームの端端で跳ね返る、どれか一つでも重なったらタイマーが止まる

--- ArrayPractice/Form1.cs
+++ ArrayPractice/Form1.cs
@@ -16,6 +16,10 @@ namespace ArrayPractice
 
         int vx = rand.Next(-20, 21);
         int vy = rand.Next(-20, 21);
+        int vx1 = rand.Next(-20, 21);
+        int vy1 = rand.Next(-20, 21);
+        int vx2 = rand.Next(-20, 21);
+        int vy2 = rand.Next(-20, 21);
         int score = 100;
 
         public Form1()
@@ -24,6 +28,10 @@ namespace ArrayPractice
 
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
+            label2.Left = rand.Next(ClientSize.Width - label1.Width);
+            label2.Top = rand.Next(ClientSize.Height - label1.Height);
+            label3.Left = rand.Next(ClientSize.Width - label1.Width);
+            label3.Top = rand.Next(ClientSize.Height - label1.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -60,6 +68,84 @@ namespace ArrayPractice
             {
                 timer1.Enabled = false;
             }
+
+            label2.Left += vx1;
+            label2.Top += vy1;
+
+            if (label2.Left < 0)
+            {
+                vx1 = Math.Abs(vx1);
+            }
+            if (label2.Top < 0)
+            {
+                vy1 = Math.Abs(vy1);
+            }
+            if (label2.Right > ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vx1);
+            }
+            if (label2.Bottom > ClientSize.Height)
+            {
+                vy1 = -Math.Abs(vy1);
+            }
+
+
+            if ((fpos.X >= label2.Left)
+                && (fpos.X < label2.Right)
+                && (fpos.Y >= label2.Top)
+                && (fpos.Y < label2.Bottom))
+            {
+                timer1.Enabled = false;
+            }
+
+            label3.Left += vx2;
+            label3.Top += vy2;
+
+            if (label3.Left < 0)
+            {
+                vx2 = Math.Abs(vx2);
+            }
+            if (label3.Top < 0)
+            {
+                vy2 = Math.Abs(vy2);
+            }
+            if (label3.Right > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx2);
+            }
+            if (label3.Bottom > ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy2);
+            }
+
+
+            if ((fpos.X >= label3.Left)
+                && (fpos.X < label3.Right)
+                && (fpos.Y >= label3.Top)
+                && (fpos.Y < label3.Bottom))
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void scoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
